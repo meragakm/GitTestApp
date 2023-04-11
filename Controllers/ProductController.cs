8cf6f89781cb92dd3ea9a4f7110c63898ab9c61a
Karim MERAGA <karim.meraga-ext@alstomgroup.com>
2023-04-11 10:35:25 +0200

    Add product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace GitTestApp.Controllers
+{
+   [Route("product")]
+   [ApiController]
+   public class ProductController : ControllerBase
+   {
+      [HttpGet]
+      public ActionResult index()
+      {
+         return Ok(1);
+      }
+
+   }
+}
--- Program.cs
+++ Program.cs
@@ -21,5 +21,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
